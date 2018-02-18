e23a4b5acfbd428de11cf70355269cb3f335006e
TahinDani <tahin.daniel@gmail.com>
2018-02-18 19:38:31 +0100

    pip errors corrected

--- Lecture8/exercise4.py
+++ Lecture8/exercise4.py
@@ -11,7 +11,7 @@ def noReplacementSimulation(numTrials):
     Runs numTrials trials of a Monte Carlo simulation
     of drawing 3 balls out of a bucket containing
     3 red and 3 green balls. Balls are not replaced once
-    drawn. Returns the a decimal - the fraction of times 3 
+    drawn. Returns the a decimal - the fraction of times 3
     balls of the same color were drawn.
     '''
     balls = [1, 1, 1, 0, 0, 0]
@@ -19,7 +19,7 @@ def noReplacementSimulation(numTrials):
     for i in range(numTrials):
         one_draw = draw_balls(balls, 3)
         result = sum(one_draw)
-        #print(one_draw, result)
+        # print(one_draw, result)
         if result == 0 or result == 3:
             success += 1
             # print("success")
